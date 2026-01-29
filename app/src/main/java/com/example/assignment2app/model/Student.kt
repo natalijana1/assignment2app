a108f3ad9131f1c9fa81d050945f193441a0dde8
liam34bar <photosdalbum@gmail.com>
2026-01-29 20:53:55 +0200

    Create basic student model

---
+++ app/src/main/java/com/example/assignment2app/model/Student.kt
@@ -0,0 +1,10 @@
+package com.example.assignment2app.model
+
+data class Student(
+    var id: String,
+    var name: String,
+    var phone: String,
+    var address: String,
+    var imageResId: Int?,
+    var isChecked: Boolean
+)
